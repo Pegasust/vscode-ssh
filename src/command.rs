915971774ceeaa8cc8293cfc28be8c5fb421f716
Pegasust <pegasucksgg@gmail.com>
2022-06-08 01:00:38 -0700

    add integration test process creation

--- src/command.rs
+++ src/command.rs
@@ -1,53 +1,9 @@
 
-use std::{format, process};
+use std::{process::{self}};
 
 use enum_dispatch::enum_dispatch;
 
-pub struct VSCodeSSH {
-    host_ip: String,
-    host_user: Option<String>,
-    host_abspath: Option<String>
-}
-
-impl VSCodeSSH {
-    // Need to acquire link: 
-    // vscode-remote://ssh-remote+felius.ddns.net/home/ubuntu_admin/git_server/vscode-ssh
-    fn get_uri(&self)->String {
-        format!("vscode-remote://ssh-remote+{}{}{}",
-            match &self.host_user {
-                Some(user) => user.clone() + "@",
-                None => "".to_string()
-            },
-            &self.host_ip,
-            self.host_abspath.clone().unwrap_or_else(|| "".to_string())
-        )
-    }
-    pub fn from_ip(host_ip: &'_ str)-> Self {
-        VSCodeSSH { host_ip: host_ip.to_string(), host_user: None, host_abspath: None }
-    }
-    pub fn from_abs(host_ip: &'_ str, host_abspath: &'_ str) -> Self {
-        VSCodeSSH { host_ip: host_ip.to_string(), host_user: None, host_abspath: Some(host_abspath.to_string()) }
-    }
-    pub fn from_user(host_ip: &'_ str, host_user: &'_ str) -> Self {
-        VSCodeSSH { host_ip: host_ip.to_string(), host_user: Some(host_user.to_string()), host_abspath: None }
-    }
-    pub fn from_all(host_ip: &'_ str, 
-        host_user: &'_ str, 
-        host_abspath: &'_ str)
-        -> Self 
-    {
-        VSCodeSSH { 
-            host_ip: host_ip.to_string(), 
-            host_user: Some(host_user.to_string()), 
-            host_abspath: Some(host_abspath.to_string())
-        }
-    }
-}
-#[enum_dispatch(Command)]
-enum CommandEnum {
-    VSCodeSSH
-}
-
+#[derive(Debug)]
 pub enum CommandOutput {
     // Command that produces nothing
     Void(Result<(), String>),
@@ -66,8 +22,32 @@ impl CommandOutput {
             a @ CommandOutput::String(_) => a,
             CommandOutput::Void(a) => 
                 CommandOutput::String(a.map(|_| "".to_string())),
-            CommandOutput::ProcessOutput(_) => todo!(),
-            a @ CommandOutput::Limbo => a,
+            CommandOutput::ProcessOutput(proc_res) => CommandOutput::String(
+                proc_res.map_err(|err| err.to_string())
+                        .map(|output| 
+                            format!("{:?}", output)
+                        )
+            ),
+            _ => CommandOutput::Limbo
+        }
+    }
+    pub fn is_limbo(&self) -> bool {
+        match self {
+            Self::Limbo => true,
+            _ => false,
+        }
+    }
+}
+
+impl Into<Result<String, String>> for CommandOutput {
+    fn into(self) -> Result<String, String> {
+        let stablized = self.stabilize();
+        match stablized {
+            CommandOutput::String(a) => a,
+            bad @ _ => Err(format!("Unable to transform: {}", match bad {
+                CommandOutput::Limbo => "Is Limbo",
+                _ => "Unknown",
+            }))
         }
     }
 }
@@ -82,50 +62,13 @@ pub trait Command {
     fn perform(&self)->CommandOutput {
         CommandOutput::ProcessOutput(
             self.apply_proc().spawn()
-            .and_then(|child| child.wait_with_output())
+                .and_then(|child| child.wait_with_output())
         )
     }
     fn drawback(&self)->Option<CommandOutput> {
-        self.undo_proc().map(|mut cmd| CommandOutput::ProcessOutput(
-            cmd.spawn().and_then(|child| child.wait_with_output())
-        ))
-    }
-}
-
-impl Command for VSCodeSSH {
-    fn apply_proc(&self) -> process::Command {
-        todo!()
-    }
-
-    fn undo_proc(&self) -> Option<process::Command> {
-        Option::None // operation not supported
-    }
-}
-
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn uri_test_c_nil_nil() {
-        assert_eq!(VSCodeSSH::from_ip("felius.ddns.net").get_uri(), 
-            "vscode-remote://ssh-remote+felius.ddns.net".to_string()
-        );
-    }
-    #[test]
-    fn uri_test_c_c() {
-        assert_eq!(VSCodeSSH::from_user("felius.ddns.net", "ubuntu_admin").get_uri(), 
-            "vscode-remote://ssh-remote+ubuntu_admin@felius.ddns.net".to_string());
-    }
-    #[test]
-    fn uri_test_c_nil_c() {
-        assert_eq!(VSCodeSSH::from_abs("felius.ddns.net", "/home/ubuntu_admin/git_server/vscode-ssh").get_uri(),
-            "vscode-remote://ssh-remote+felius.ddns.net/home/ubuntu_admin/git_server/vscode-ssh".to_string()
-        );
-    }
-    #[test]
-    fn uri_test_c_c_c() {
-        assert_eq!(VSCodeSSH::from_all("felius.ddns.net", "ubuntu_admin", "/home/ubuntu_admin/git_server").get_uri(),
-        "vscode-remote://ssh-remote+ubuntu_admin@felius.ddns.net/home/ubuntu_admin/git_server".to_string());
+        self.undo_proc()
+            .map(|mut cmd| CommandOutput::ProcessOutput(
+                cmd.spawn().and_then(|child| child.wait_with_output())
+            ))
     }
 }
--- tests/test_commands.rs
+++ tests/test_commands.rs
@@ -1,5 +1,25 @@
 
+use vscode_ssh::{CommandEnum::{*, self}, VSCodeSSH, command::Command};
+
+fn ssh_local() -> VSCodeSSH {
+    VSCodeSSH::from_ip("localhost")
+}
+
+fn ssh_felius() -> VSCodeSSH {
+    VSCodeSSH::from_auth(
+        "felius.ddns.net", 
+        "ubuntu_admin", 
+        "/home/ubuntu_admin/git_server/vscode-ssh"
+    )
+}
+
 #[test]
 fn vs_ssh_cannot_undo() {
-    VSCodeSSH
+    let ssh_subcmd: CommandEnum = ssh_felius().into();
+    assert!(ssh_subcmd.undo_proc().is_none());
+}
+
+#[test]
+fn vs_ssh_perform_end_well() {
+    assert!(!ssh_felius().perform().stabilize().is_limbo())
 }
